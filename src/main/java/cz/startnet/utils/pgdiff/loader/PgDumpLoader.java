44bd60df938ab71549d2579919f2d323b36ad7da
Miroslav Šulc <fordfrog@fordfrog.com>
2012-06-01 22:13:23 +0200

    reformatted

--- src/main/java/cz/startnet/utils/pgdiff/loader/PgDumpLoader.java
+++ src/main/java/cz/startnet/utils/pgdiff/loader/PgDumpLoader.java
@@ -17,7 +17,6 @@ import cz.startnet.utils.pgdiff.parsers.CreateTableParser;
 import cz.startnet.utils.pgdiff.parsers.CreateTriggerParser;
 import cz.startnet.utils.pgdiff.parsers.CreateViewParser;
 import cz.startnet.utils.pgdiff.schema.PgDatabase;
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -26,7 +25,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.text.MessageFormat;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
