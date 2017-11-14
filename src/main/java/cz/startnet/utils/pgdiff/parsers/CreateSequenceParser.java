b51adefb6161116022264df212211773a9cbdfb4
Jalisson Mello <jalissonmeneses@hotmail.com>
2017-11-13 21:55:16 -0300

    Create Sequence Postgres 10

--- src/main/java/cz/startnet/utils/pgdiff/parsers/CreateSequenceParser.java
+++ src/main/java/cz/startnet/utils/pgdiff/parsers/CreateSequenceParser.java
@@ -45,7 +45,9 @@ public class CreateSequenceParser {
         schema.addSequence(sequence);
 
         while (!parser.expectOptional(";")) {
-            if (parser.expectOptional("INCREMENT")) {
+            if (parser.expectOptional("AS")) {
+                sequence.setDataType(parser.parseString());
+            } else if (parser.expectOptional("INCREMENT")) {
                 parser.expectOptional("BY");
                 sequence.setIncrement(parser.parseString());
             } else if (parser.expectOptional("MINVALUE")) {
