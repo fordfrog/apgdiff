6e69e2574eb68215d6d52aec8158a328748d2ea1
jalisson <jalisson@skynet.lan>
2019-03-26 21:01:51 -0300

    Drop e Add constraint if table exists

--- src/test/resources/cz/startnet/utils/pgdiff/add_constraint_diff.sql
+++ src/test/resources/cz/startnet/utils/pgdiff/add_constraint_diff.sql
@@ -1,3 +1,3 @@
 
-ALTER TABLE testtable
+ALTER TABLE IF EXISTS testtable
 	ADD CONSTRAINT IF NOT EXISTS field4check CHECK (((field4 > (-5.0)::double precision) AND (field4 < (5.0)::double precision)));
--- src/test/resources/cz/startnet/utils/pgdiff/modify_constraint_diff.sql
+++ src/test/resources/cz/startnet/utils/pgdiff/modify_constraint_diff.sql
@@ -1,6 +1,6 @@
 
-ALTER TABLE testtable
+ALTER TABLE IF EXISTS testtable
 	DROP CONSTRAINT IF EXISTS field4check;
 
-ALTER TABLE testtable
+ALTER TABLE IF EXISTS testtable
 	ADD CONSTRAINT IF NOT EXISTS field4check CHECK ((field4 > (0.0)::double precision));
